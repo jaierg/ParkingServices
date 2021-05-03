7c7feba78afd6e91a35d3bfcb51c7197e34dbad1
Abu Nadim <nadimkabir2@gmail.com>
2021-05-02 22:04:17 -0400

    create inset.sql and testQueries.sql for parkingServices application

---
+++ SQL/insert.sql
@@ -0,0 +1,86 @@
+INSERT INTO lot VALUES("1","456 Ralph McGill Blvd NE, Atlanta, GA 30312","LAZ Parking Lot");
+INSERT INTO lot VALUES("2","999 Metropolis Alley, Atlanta, GA 30309","Metropolis Parking");
+INSERT INTO lot VALUES("3","2 North Avenue NE, Atlanta, GA 30308","Eagle Parking");
+
+INSERT INTO spot VALUES("1","1",5,2,"6.00","12.00","2021-05-15");
+INSERT INTO spot VALUES("2","2",4,2,"4.00","8.00","2021-05-15");
+INSERT INTO spot VALUES("3","3",6,2,"5.00","10.00","2021-05-15");
+INSERT INTO spot VALUES("4","1",5,2,"6.00","12.00","2021-05-16");
+INSERT INTO spot VALUES("5","1",5,2,"6.00","12.00","2021-05-17");
+INSERT INTO spot VALUES("6","2",4,2,"4.00","8.00","2021-05-16");
+INSERT INTO spot VALUES("7","2",4,2,"4.00","8.00","2021-05-17");
+INSERT INTO spot VALUES("8","3",6,2,"5.00","10.00","2021-05-16");
+INSERT INTO spot VALUES("9","3",6,2,"5.00","10.00","2021-05-17");
+
+INSERT INTO reservation VALUES("1","1","14:00:00","16:00:00","vip","10");
+INSERT INTO reservation VALUES("2","1","12:00:00","14:00:00","vip","11");
+INSERT INTO reservation VALUES("3","1","08:00:00","10:00:00","std","150");
+INSERT INTO reservation VALUES("4","1","10:00:00","12:00:00","std","55");
+INSERT INTO reservation VALUES("5","1","12:00:00","14:00:00","std","79");
+INSERT INTO reservation VALUES("6","1","14:00:00","16:00:00","std","124");
+INSERT INTO reservation VALUES("7","1","16:00:00","18:00:00","std","47");
+
+INSERT INTO reservation VALUES("8","2","10:00:00","12:00:00","vip","9");
+INSERT INTO reservation VALUES("9","2","14:00:00","16:00:00","vip","10");
+INSERT INTO reservation VALUES("10","2","10:00:00","12:00:00","std","35");
+INSERT INTO reservation VALUES("11","2","14:00:00","16:00:00","std","21");
+INSERT INTO reservation VALUES("12","2","16:00:00","18:00:00","std","22");
+INSERT INTO reservation VALUES("13","2","18:00:00","20:00:00","std","95");
+
+INSERT INTO reservation VALUES("14","3","10:00:00","12:00:00","vip","8");
+INSERT INTO reservation VALUES("15","3","14:00:00","16:00:00","vip","9");
+INSERT INTO reservation VALUES("16","3","10:00:00","12:00:00","std","81");
+INSERT INTO reservation VALUES("17","3","14:00:00","16:00:00","std","23");
+INSERT INTO reservation VALUES("18","3","16:00:00","18:00:00","std","42");
+INSERT INTO reservation VALUES("19","3","08:00:00","10:00:00","std","21");
+INSERT INTO reservation VALUES("20","3","12:00:00","14:00:00","std","95");
+INSERT INTO reservation VALUES("21","3","18:00:00","20:00:00","std","21");
+
+INSERT INTO reservation VALUES("22","4","14:00:00","16:00:00","vip","5");
+INSERT INTO reservation VALUES("23","4","12:00:00","14:00:00","vip","8");
+INSERT INTO reservation VALUES("24","4","08:00:00","10:00:00","std","57");
+INSERT INTO reservation VALUES("25","4","10:00:00","12:00:00","std","41");
+INSERT INTO reservation VALUES("26","4","12:00:00","14:00:00","std","39");
+INSERT INTO reservation VALUES("27","4","14:00:00","16:00:00","std","19");
+INSERT INTO reservation VALUES("28","4","16:00:00","18:00:00","std","89");
+
+INSERT INTO reservation VALUES("29","5","14:00:00","16:00:00","vip","8");
+INSERT INTO reservation VALUES("30","5","12:00:00","14:00:00","vip","9");
+INSERT INTO reservation VALUES("31","5","08:00:00","10:00:00","std","32");
+INSERT INTO reservation VALUES("32","5","10:00:00","12:00:00","std","41");
+INSERT INTO reservation VALUES("33","5","12:00:00","14:00:00","std","79");
+INSERT INTO reservation VALUES("34","5","14:00:00","16:00:00","std","17");
+INSERT INTO reservation VALUES("35","5","16:00:00","18:00:00","std","75");
+
+INSERT INTO reservation VALUES("36","6","10:00:00","12:00:00","vip","2");
+INSERT INTO reservation VALUES("37","6","14:00:00","16:00:00","vip","3");
+INSERT INTO reservation VALUES("38","6","10:00:00","12:00:00","std","47");
+INSERT INTO reservation VALUES("39","6","14:00:00","16:00:00","std","142");
+INSERT INTO reservation VALUES("40","6","16:00:00","18:00:00","std","74");
+INSERT INTO reservation VALUES("41","6","18:00:00","20:00:00","std","131");
+
+INSERT INTO reservation VALUES("42","7","10:00:00","12:00:00","vip","2");
+INSERT INTO reservation VALUES("43","7","14:00:00","16:00:00","vip","3");
+INSERT INTO reservation VALUES("44","7","10:00:00","12:00:00","std","24");
+INSERT INTO reservation VALUES("45","7","14:00:00","16:00:00","std","28");
+INSERT INTO reservation VALUES("46","7","16:00:00","18:00:00","std","127");
+INSERT INTO reservation VALUES("47","7","18:00:00","20:00:00","std","21");
+
+INSERT INTO reservation VALUES("48","8","10:00:00","12:00:00","vip","8");
+INSERT INTO reservation VALUES("49","8","14:00:00","16:00:00","vip","9");
+INSERT INTO reservation VALUES("50","8","10:00:00","12:00:00","std","54");
+INSERT INTO reservation VALUES("51","8","14:00:00","16:00:00","std","87");
+INSERT INTO reservation VALUES("52","8","16:00:00","18:00:00","std","31");
+INSERT INTO reservation VALUES("53","8","08:00:00","10:00:00","std","122");
+INSERT INTO reservation VALUES("54","8","12:00:00","14:00:00","std","100");
+INSERT INTO reservation VALUES("55","8","18:00:00","20:00:00","std","47");
+
+INSERT INTO reservation VALUES("56","9","10:00:00","12:00:00","vip","8");
+INSERT INTO reservation VALUES("57","9","14:00:00","16:00:00","vip","9");
+INSERT INTO reservation VALUES("58","9","10:00:00","12:00:00","std","145");
+INSERT INTO reservation VALUES("59","9","14:00:00","16:00:00","std","64");
+INSERT INTO reservation VALUES("60","9","16:00:00","18:00:00","std","40");
+INSERT INTO reservation VALUES("61","9","08:00:00","10:00:00","std","14");
+INSERT INTO reservation VALUES("62","9","12:00:00","14:00:00","std","15");
+INSERT INTO reservation VALUES("63","9","18:00:00","20:00:00","std","51");
+
